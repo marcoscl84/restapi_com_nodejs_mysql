a39c25797a57ef3ba1c7d5f0bef29eef07ccbbbe
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-07-16 13:35:16 -0300

    Nodemon, morgan e tratamento de erro de rota

--- app.js
+++ app.js
@@ -1,11 +1,29 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const rotaProdutos = require('./routes/produtos')
 const rotaPedidos = require('./routes/pedidos')
 
+// MONITORA EXECUÇÃO DAS ROTAS E DEVOLVE UM LOG NO CONSOLE
+app.use(morgan('dev'));
+
 app.use('/produtos', rotaProdutos);
 app.use('/pedidos', rotaPedidos);
 
+// QUANDO NÃO ENCONTRA A ROTA CHAMADA, entra nessa:
+app.use((req, res, next) => {
+    const erro = new Error('Não encontrado');
+    erro.status = 404;
+    next(erro);
+})
+app.use((error, req, res, next) => {
+    res.status(error.status || 500)
+    return res.send({
+        erro: {
+            mensagem: error.message
+        }
+    })
+})
 
 module.exports = app;
