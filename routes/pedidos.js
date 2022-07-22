ac1c3fe98c3eeb88af1bda1ffb285c8bf71de892
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-07-22 15:01:58 -0300

    feat: relacionamento tabelas

--- routes/pedidos.js
+++ routes/pedidos.js
@@ -4,34 +4,46 @@ const mysql = require('../mysql').pool
 
 // RETORNA TODOS OS PEDIDOS
 router.get('/', (req, res, next) => {
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error })
-    }
-    conn.query('SELECT * FROM pedidos', (error, result, fields) => {
-      if (error) {
+    mysql.getConnection((error, conn) => {
+        if (error) {
         return res.status(500).send({ error: error })
-      }
+        }
+        conn.query(`SELECT pedidos.id_pedido, 
+                            pedidos.quantidade, 
+                            produtos.id_produto, 
+                            produtos.nome, 
+                            produtos.preco 
+                    FROM pedidos 
+                    INNER JOIN produtos 
+                    ON produtos.id_produto = pedidos.produtos_id_produto`, 
+        (error, result, fields) => {
+        if (error) {
+            return res.status(500).send({ error: error })
+        }
 
-      const response = {
-        quantidade: result.length,
-        pedidos: result.map(pedido => {
-          return {
-            id_pedido: pedido.id_pedido,
-            produtos_id_produto: pedido.produtos_id_produto,
-            quantidade: pedido.id_pedido,
-            request: {
-              tipo: 'GET',
-              descricao: `Retorna os dados do pedido => ${pedido.id_pedido}`,
-              URL: 'http://localhost:3001/produtos/' + pedido.id_pedido
+        const response = {
+            quantidade: result.length,
+            pedidos: result.map(pedido => {
+            return {
+                id_pedido: pedido.id_pedido,
+                quantidade: pedido.id_pedido,
+                produto: {
+                    produtos_id_produto: pedido.produtos_id_produto,
+                    nome: pedido.nome,
+                    preco: pedido.preco
+                },
+                request: {
+                tipo: 'GET',
+                descricao: `Retorna os dados do pedido => ${pedido.id_pedido}`,
+                URL: 'http://localhost:3001/produtos/' + pedido.id_pedido
+                }
             }
-          }
-        })
-      }
+            })
+        }
 
-      return res.status(200).send({ response })
+        return res.status(200).send({ response })
+        })
     })
-  })
 })
 
 // INSERE UM PEDIDO
@@ -85,78 +97,78 @@ router.post('/', (req, res, next) => {
 
 // RETORNA OS DADOS DE UM PEDIDO
 router.get('/:id_pedido', (req, res, next) => {
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error })
-    }
-    conn.query(
-      'SELECT * FROM pedidos WHERE id_pedido = ?',
-      [req.params.id_pedido],
-      (error, result, fields) => {
+    mysql.getConnection((error, conn) => {
         if (error) {
-          return res.status(500).send({ error: error })
+        return res.status(500).send({ error: error })
         }
-
-        if (result.length == 0) {
-          return res.status(404).send({
-            mensagem: 'Não foi encontrado nenhum pedido com este ID'
-          })
-        }
-
-        const response = {
-          pedido: {
-            id_pedido: result[0].id_pedido,
-            id_produto: result[0].id_produto,
-            quantidade: result[0].quantidade,
-            request: {
-              tipo: 'GET',
-              descricao: 'Retorna os detalhes de um pedido específico',
-              URL: 'http://localhost:3001/pedidos/'
+        conn.query(
+        'SELECT * FROM pedidos WHERE id_pedido = ?',
+        [req.params.id_pedido],
+        (error, result, fields) => {
+            if (error) {
+            return res.status(500).send({ error: error })
             }
-          }
-        }
 
-        return res.status(200).send(response)
-      }
-    )
-  })
+            if (result.length == 0) {
+            return res.status(404).send({
+                mensagem: 'Não foi encontrado nenhum pedido com este ID'
+            })
+            }
+
+            const response = {
+            pedido: {
+                id_pedido: result[0].id_pedido,
+                id_produto: result[0].id_produto,
+                quantidade: result[0].quantidade,
+                request: {
+                tipo: 'GET',
+                descricao: 'Retorna os detalhes de um pedido específico',
+                URL: 'http://localhost:3001/pedidos/'
+                }
+            }
+            }
+
+            return res.status(200).send(response)
+        }
+        )
+    })
 })
 
 // EXCLUI UM PEDIDO
 router.delete('/', (req, res, next) => {
-  mysql.getConnection((error, conn) => {
-    if (error) {
-      return res.status(500).send({ error: error })
-    }
-    conn.query(
-      `DELETE FROM pedidos
-                WHERE id_pedido = ?`,
-      [req.body.id_pedido],
-      (error, result, field) => {
-        // liberar o pool pra não estourar o limite de conexões
-        conn.release()
-
+    mysql.getConnection((error, conn) => {
         if (error) {
-          return res.status(500).send({ error: error })
+            return res.status(500).send({ error: error })
         }
+        conn.query(
+        `DELETE FROM pedidos
+                    WHERE id_pedido = ?`,
+        [req.body.id_pedido],
+        (error, result, field) => {
+            // liberar o pool pra não estourar o limite de conexões
+            conn.release()
 
-        const response = {
-          mensagem: 'Pedido removido com sucesso!',
-          request: {
-            tipo: 'DELETE',
-            descricao: `Produto ${req.body.id_pedido} excluído`,
-            url: 'http://localhost:3001/pedidos/',
-            body: {
-              produtos_id_produto: 'Number',
-              quantidade: 'Number'
+            if (error) {
+            return res.status(500).send({ error: error })
             }
-          }
-        }
 
-        res.status(202).send(response)
-      }
-    )
-  })
+            const response = {
+            mensagem: 'Pedido removido com sucesso!',
+            request: {
+                tipo: 'DELETE',
+                descricao: `Produto ${req.body.id_pedido} excluído`,
+                url: 'http://localhost:3001/pedidos/',
+                body: {
+                    produtos_id_produto: 'Number',
+                    quantidade: 'Number'
+                    }
+                }
+            }
+
+            res.status(202).send(response)
+        }
+        )
+    })
 })
 
 module.exports = router
