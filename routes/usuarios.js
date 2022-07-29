5a54f659d33a69b68165bc74a3c659ef173eae56
Marcos Conte Lima <marcoscontelima@yahoo.com.br>
2022-07-28 21:49:44 -0300

    refactor: correções no código para add usuário

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -2,33 +2,37 @@ const express = require('express');
 const router = express.Router();
 const mysql = require('../mysql').pool;
 const bcrypt = require('bcrypt');
-const { response } = require('express');
 
-// CRIAR USUÁRIO COM SENHA HASH
 router.post('/cadastro', (req, res, next) => {
     mysql.getConnection((err, conn) => {
-
         if (err) { return res.status(500).send({ error: error }) }
-        bcrypt.hash(req.body.senha, 10, (errbcrypt, hash) =>{
-
-            if(errbcrypt){ return res.status(500).send({ error: errbcrypt }) }
-            conn.query(
-                'INSERT INTO usuarios (email, senha) VALUES (?,?)', 
-                    [req.body.email, hash]),
-                    (error, results) => {
-                    conn.release();
-                    if(error) { return res.status(500).send({ error: error }) }
-                    response = {                   
-                        mensagem: 'Usuário criado com sucesso!',
-                        usuarioCriado: {
-                            id_usuario: results.insertId,
-                            email: req.body.email
-                        }                   
-                    }
-                    return res.status(201).send(response);                   
-                }
+        conn.query('SELECT * FROM usuarios WHERE email = ?', [req.body.email], (error, results) => {
+            if (error) { return res.status(500).send({ error: error }) }
+            if (results.length > 0) {
+                res.status(409).send({ mensagem: 'Usuário já cadastrado' })
+            } else {
+                bcrypt.hash(req.body.senha, 10, (errBcrypt, hash) => {
+                    if (errBcrypt) { return res.status(500).send({ error: errBcrypt }) }
+                    conn.query(
+                        `INSERT INTO usuarios (email, senha) VALUES (?,?)`,
+                        [req.body.email, hash],
+                        (error, results) => {
+                            conn.release();
+                            if (error) { return res.status(500).send({ error: error }) }
+                            response = {
+                                mensagem: 'Usuário criado com sucesso',
+                                usuarioCriado: {
+                                    id_usuario: results.insertId,
+                                    email: req.body.email
+                                }
+                            }
+                            return res.status(201).send(response);
+                        })
+                });
+            }
         })
-    })
+
+    });
 })
 
-module.exports = router;
+module.exports = router; 
